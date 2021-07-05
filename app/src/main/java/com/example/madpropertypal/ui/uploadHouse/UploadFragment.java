6aafbbb371c698272b28abc8a932b308f672fb25
Dennis <56588737+Donn-MXI@users.noreply.github.com>
2021-07-05 13:17:44 +0300

    added property addition confirmation layout

--- app/src/main/java/com/example/madpropertypal/ui/uploadHouse/UploadFragment.java
+++ app/src/main/java/com/example/madpropertypal/ui/uploadHouse/UploadFragment.java
@@ -1,11 +1,16 @@
 package com.example.madpropertypal.ui.uploadHouse;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -98,7 +103,9 @@ public class UploadFragment extends Fragment implements View.OnClickListener {
     //check if the required inputs are filled or not
     // in order to disable button
 
-    private String propertyName, propertyType, leaseType, location, amenities, description, noOfBedrooms, noOfBathrooms, size, price;
+    private String propertyName, propertyType, leaseType, location,
+            amenities, description, noOfBedrooms, noOfBathrooms, size, price;
+
     private void checkRequiredFieldsForEmptyValues() {
 
 
@@ -138,8 +145,70 @@ public class UploadFragment extends Fragment implements View.OnClickListener {
 
 
         if (v == button){
-        Toast.makeText(getContext(), "button clicked", Toast.LENGTH_SHORT).show();
+
+            Toast.makeText(getContext(), "button clicked", Toast.LENGTH_SHORT).show();
+            openDialog(R.layout.dialog_layout);
+
+        }else if (v == cancelBT){
+
+
+            Toast.makeText(getContext(), "Make your changes then submit again", Toast.LENGTH_SHORT).show();
+            dialog.dismiss();
+
+        } else if (v == confirmBT) {
+
+            Toast.makeText(getContext(), "Confirmed", Toast.LENGTH_SHORT).show();
 
         }
+    }
+
+
+
+
+
+
+
+
+
+
+    //adding the confirmation dialog
+    Dialog dialog;
+    TextView nameTV, typeTV, leaseTypeTV, locationTV, localAmenitiesTV,
+            descriptionTV, bathroomNumber, bedroomNumber,sizeTV;
+    Button confirmBT, cancelBT;
+    public void openDialog(int layoutID){
+
+        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+        final View inflator = layoutInflater.inflate(layoutID, null);
+
+        dialog = new Dialog(getContext());
+        Window window = dialog.getWindow();
+        window.setGravity(Gravity.CENTER);
+
+        dialog.setTitle(null);
+        dialog.setContentView(R.layout.dialog_layout);
+        dialog.setContentView(inflator);
+
+        //selection buttons onclick
+        nameTV =  inflator.findViewById(R.id.nameTV);
+        typeTV =  inflator.findViewById(R.id.nameTV);
+
+        
+        nameTV.setText(propertyName);
+        typeTV.setText(propertyType);
+
+
+        cancelBT = inflator.findViewById(R.id.cancelBT);
+        cancelBT.setOnClickListener(this::onClick);
+        confirmBT = inflator.findViewById(R.id.confirmBT);
+        confirmBT.setOnClickListener(this::onClick);
+
+        dialog.show();
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.copyFrom(dialog.getWindow().getAttributes());
+        layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        dialog.getWindow().setAttributes(layoutParams);
+
     }
 }
