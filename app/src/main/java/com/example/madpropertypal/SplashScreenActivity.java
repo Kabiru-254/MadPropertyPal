6b3b50eb847a2dbf3655136081f68ed0dd5558c2
Dennis <56588737+Donn-MXI@users.noreply.github.com>
2021-07-08 20:50:27 +0300

    added dotted progress

--- app/src/main/java/com/example/madpropertypal/SplashScreenActivity.java
+++ app/src/main/java/com/example/madpropertypal/SplashScreenActivity.java
@@ -5,12 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     private Button Next;
     private Handler handler = new Handler();
@@ -20,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
 
 
 
@@ -29,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
                 startActivity(intent);
                 finish();
             }
