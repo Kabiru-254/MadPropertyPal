7ce0bb6a942dc779d5762c02ec0821085cb45898
Dennis <56588737+Donn-MXI@users.noreply.github.com>
2021-07-13 21:56:55 +0300

    post button

--- android/app/src/main/java/com/example/madpropertypal/ui/home/HomeFragment.java
+++ android/app/src/main/java/com/example/madpropertypal/ui/home/HomeFragment.java
@@ -90,99 +90,11 @@ public class HomeFragment extends Fragment {
 
         SQLiteHelper sqLiteHelpar = new SQLiteHelper(getContext(), "HOUSES_AVAILABLE");
         list = sqLiteHelpar.fetchHouses();
-        uploadJSON(list);
         mAdapter = new RecyclerViewAdapter(getContext(),list);
         recyclerView.setAdapter(mAdapter);
     }
 
 
 
-
-
-
-    public void uploadJSON(List<PropertyModel> listq) {
-
-        JSONObject jsonObject = new JSONObject();
-        JSONArray jsonArray = new JSONArray();
-
-        int i = 0;
-        for (PropertyModel propertyModel : listq) {
-
-            JSONObject jsonObj= new JSONObject();
-            try {
-                jsonObj.put("id", propertyModel.getId());
-                jsonObj.put("name", propertyModel.getPropertyName());
-                jsonObj.put("type", propertyModel.getPropertyType());
-                jsonObj.put("location", propertyModel.getLocation());
-                jsonObj.put("Number of bedrooms", propertyModel.getBedroomNumber());
-                jsonObj.put("number of bathrooms", propertyModel.getBathroomNumber());
-                jsonObj.put("price", propertyModel.getAskingPrice());
-                jsonObj.put("rooms", propertyModel.getBedroomNumber());
-                jsonObj.put("size", propertyModel.getSize());
-                jsonObj.put("amenities", propertyModel.getLocalAmenities());
-                jsonObj.put("description", propertyModel.getDescription());
-
-                jsonArray.put(jsonObj);
-                jsonObject.put("userId", JSonClass.USER_ID);
-                jsonObject.put("detailList",jsonArray);
-
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-          StringRequest stringRequest = new StringRequest(Request.Method.POST
-                , JSonClass.POST_URL, response -> {
-            try {
-
-               // Log.i("RESPONSE",response.toString());
-
-                JSONObject jsonObject1 = new JSONObject(response);
-                if (jsonObject1.getString("uploadResponseCode").equals("SUCCESS")) {
-                    String message = jsonObject1.getString("message");
-                    String userid = jsonObject1.getString("userid");
-                    String names = jsonObject1.getString("names");
-                    int number = jsonObject1.getInt("number");
-                    Toast.makeText(getContext(), "Success: Successful "
-                            +"\n userId: "+ userid
-                            +"\n Names of Uploaded Properties: "+names
-                            +"\n Uploaded Properties:"+ number
-                            +"\n Message"+ message, Toast.LENGTH_SHORT).show();
-
-                }else {
-
-                    String message = jsonObject1.getString("message");
-                    Toast.makeText(getContext(), "Success: Failed" +" \n Message: "+ message, Toast.LENGTH_SHORT).show();
-                }
-
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-
-        }, error -> {
-
-            error.printStackTrace();
-
-        }) {
-
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                HashMap map = new HashMap<>();
-                map.put("jsonpayload",jsonObject.toString());
-
-                return map;
-            }
-
-
-        };
-
-
-        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
-        requestQueue.add(stringRequest);
-    }
 
 }
--- android/app/src/main/java/com/example/madpropertypal/ui/myproperties/PropertiesFragment.java
+++ android/app/src/main/java/com/example/madpropertypal/ui/myproperties/PropertiesFragment.java
@@ -13,33 +13,60 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.madpropertypal.JSonClass;
 import com.example.madpropertypal.R;
 import com.example.madpropertypal.adapters.EditorsRecyclerViewAdapter;
 import com.example.madpropertypal.adapters.RecyclerViewAdapter;
 import com.example.madpropertypal.models.PropertyModel;
 import com.example.madpropertypal.sqlite.SQLiteHelper;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PropertiesFragment extends Fragment {
 
     RecyclerView recyclerView;
     List<PropertyModel> list=new ArrayList<>();
     RecyclerView.Adapter mAdapter;
+    private Button postBT;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
          View root = inflater.inflate(R.layout.properties_fragment, container, false);
 
+         postBT = root.findViewById(R.id.postBT);
+
          recyclerView = root.findViewById(R.id.recyclerView4);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+         recyclerView.setHasFixedSize(true);
+         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
 
         fetchItems();
+
+
+        postBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                uploadJSON(list);
+
+            }
+        });
          return root;
     }
 
@@ -54,6 +81,101 @@ public class PropertiesFragment extends Fragment {
 
         mAdapter = new EditorsRecyclerViewAdapter(getContext(),list);
         recyclerView.setAdapter(mAdapter);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public void uploadJSON(List<PropertyModel> listq) {
+
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+
+        int i = 0;
+        for (PropertyModel propertyModel : listq) {
+
+            JSONObject jsonObj= new JSONObject();
+            try {
+                jsonObj.put("id", propertyModel.getId());
+                jsonObj.put("name", propertyModel.getPropertyName());
+                jsonObj.put("type", propertyModel.getPropertyType());
+                jsonObj.put("location", propertyModel.getLocation());
+                jsonObj.put("Number of bedrooms", propertyModel.getBedroomNumber());
+                jsonObj.put("number of bathrooms", propertyModel.getBathroomNumber());
+                jsonObj.put("price", propertyModel.getAskingPrice());
+                jsonObj.put("rooms", propertyModel.getBedroomNumber());
+                jsonObj.put("size", propertyModel.getSize());
+                jsonObj.put("amenities", propertyModel.getLocalAmenities());
+                jsonObj.put("description", propertyModel.getDescription());
+
+                jsonArray.put(jsonObj);
+                jsonObject.put("userId", JSonClass.USER_ID);
+                jsonObject.put("detailList",jsonArray);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST
+                , JSonClass.POST_URL, response -> {
+            try {
+
+                // Log.i("RESPONSE",response.toString());
+
+                JSONObject jsonObject1 = new JSONObject(response);
+                if (jsonObject1.getString("uploadResponseCode").equals("SUCCESS")) {
+                    String message = jsonObject1.getString("message");
+                    String userid = jsonObject1.getString("userid");
+                    String names = jsonObject1.getString("names");
+                    int number = jsonObject1.getInt("number");
+                    Toast.makeText(getContext(), "Success: Successful "
+                            +"\n userId: "+ userid
+                            +"\n Names of Uploaded Properties: "+names
+                            +"\n Uploaded Properties:"+ number
+                            +"\n Message"+ message, Toast.LENGTH_SHORT).show();
+
+                }else {
+
+                    String message = jsonObject1.getString("message");
+                    Toast.makeText(getContext(), "Success: Failed" +" \n Message: "+ message, Toast.LENGTH_SHORT).show();
+                }
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }, error -> {
+
+            error.printStackTrace();
+
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap map = new HashMap<>();
+                map.put("jsonpayload",jsonObject.toString());
+
+                return map;
+            }
+
+
+        };
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
     }
 
 
