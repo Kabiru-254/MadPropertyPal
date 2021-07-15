ba649039e4e33bcf30bd2835e748859761413279
Kabiru <kabiruworld@gmail.com>
2021-07-16 00:05:59 +0300

    SplashScreen

--- android/app/src/main/java/com/example/madpropertypal/SplashScreenActivity.java
+++ android/app/src/main/java/com/example/madpropertypal/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.madpropertypal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,11 +8,15 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashScreenActivity extends AppCompatActivity {
 
     private Button Next;
     private Handler handler = new Handler();
     private int SLEEP_DURATION = 3000;
+    String UserID;
 
 
     @Override
@@ -19,22 +24,43 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        UserID = FirebaseAuth.getInstance().getUid();
 
-
-
-        //sleep for 3 seconds then move to home page;
-        handler.postDelayed(new Runnable() {
+        final FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
-                startActivity(intent);
-                finish();
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser =mAuth.getCurrentUser();
+
+                //sleep for 3 seconds then move to home page;
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        if (firebaseUser==null)
+                        {
+                            Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+
+                            Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+
+                        }
+
+                    }
+                },SLEEP_DURATION);
+
+
+
+
             }
-        },SLEEP_DURATION);
+        };
 
-
-
-
+        authStateListener.onAuthStateChanged(mAuth);
 
     }
 }
